b2affb815ead2dc02fa8281537b490465fc4a94f
ashish-greycube <mr.ashish.shah@gmail.com>
2021-08-02 11:09:43 +0200

    feat: add custom doctypes

---
+++ amasi/amasi/doctype/item_replacement_ct/item_replacement_ct.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Greycube and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Replacement CT', {
+	// refresh: function(frm) {
+
+	// }
+});
