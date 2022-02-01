a41d1ad76f9590ee694e3861c01ccda49bcc8eb8
vr-greycube <vijay@greycube.in>
2022-02-01 17:34:31 +0530

    feat: item replacement changes
    validate batch_no and serial_no
    set qty from serial no

--- amasi/amasi/doctype/item_replacement_ct/item_replacement_ct.js
+++ amasi/amasi/doctype/item_replacement_ct/item_replacement_ct.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on("Item Replacement CT", {
   setup: function (frm) {
+    // allow only stock items in item_returned child table
     frm.set_query("item_code", "item_returned", function (doc, cdt, cdn) {
       let row = locals[cdt][cdn];
       return {
@@ -11,8 +12,62 @@ frappe.ui.form.on("Item Replacement CT", {
         },
       };
     });
+
+    frm.set_query("batch_no", "item_returned", function (doc, cdt, cdn) {
+      let row = locals[cdt][cdn];
+      return {
+        filters: {
+          item: row['item_code'],
+        },
+      };
+    });
+
+    frm.set_query("batch_no", "item_issued", function (doc, cdt, cdn) {
+      let row = locals[cdt][cdn];
+      return {
+        filters: {
+          item: row['item_code'],
+        },
+      };
+    });
+
   },
+
+  validate: function (frm) {
+    // 
+  },
+
+
 });
+
+
+frappe.ui.form.on(
+  "Item Returned",
+  "item_code",
+  function (frm, cdt, cdn) {
+    let row_item = locals[cdt][cdn];
+    frappe.model.with_doc("Item", row_item.item_code, function () {
+      let item = frappe.get_doc("Item", row_item.item_code);
+      var grid_row = frm.open_grid_row() || frm.get_field('item_returned').grid.get_row(cdn);
+      grid_row.toggle_reqd("serial_no", item.has_serial_no == 0 ? false : true);
+      grid_row.toggle_reqd("batch_no", item.has_batch_no == 0 ? false : true);
+    });
+  }
+);
+
+frappe.ui.form.on(
+  "Item Issued",
+  "item_code",
+  function (frm, cdt, cdn) {
+    let row_item = locals[cdt][cdn];
+    frappe.model.with_doc("Item", row_item.item_code, function () {
+      let item = frappe.get_doc("Item", row_item.item_code);
+      var grid_row = frm.open_grid_row() || frm.get_field('item_returned').grid.get_row(cdn);
+      grid_row.toggle_reqd("serial_no", item.has_serial_no == 0 ? false : true);
+      grid_row.toggle_reqd("batch_no", item.has_batch_no == 0 ? false : true);
+    });
+  }
+);
 
 frappe.ui.form.on(
   "Item Returned",
@@ -24,11 +79,10 @@ frappe.ui.form.on(
 );
 
 frappe.ui.form.on(
-    "Item Issued",
-    "item_issued_add",
-    function (frm, cdt, cdn) {
-      let row = locals[cdt][cdn];
-      row.warehouse = frm.doc.warehouse;
-    }
-  );
-  
+  "Item Issued",
+  "item_issued_add",
+  function (frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    row.warehouse = frm.doc.warehouse;
+  }
+);
--- amasi/amasi/doctype/item_replacement_ct/item_replacement_ct.py
+++ amasi/amasi/doctype/item_replacement_ct/item_replacement_ct.py
@@ -3,12 +3,53 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from distutils.log import debug
 import frappe
 from frappe.model.document import Document
 from erpnext import get_company_currency, get_default_company
 
 
 class ItemReplacementCT(Document):
+    def validate(self):
+        msg = []
+        for d in self.item_returned + self.item_issued:
+            item = frappe.get_doc("Item", d.item_code)
+            if item.has_serial_no and not d.serial_no:
+                msg.append(
+                    "Serial No missing for item %s" % (frappe.bold(item.item_name))
+                )
+            if item.has_batch_no and not d.batch_no:
+                msg.append(
+                    "Batch No missing for item %s" % (frappe.bold(item.item_name))
+                )
+            if d.serial_no:
+                d.qty = d.serial_no and len(d.serial_no.split("\n")) or 0
+                if d.doctype == "Item Issued":
+                    self.validate_serial_numbers(d.serial_no, d.item_code)
+
+        if msg:
+            frappe.throw("\n".join(msg))
+
+    def validate_serial_numbers(self, serial_no, item_code):
+        serial_nos = serial_no.split("\n")
+        valid_numbers = frappe.db.sql(
+            """
+            select name
+            from `tabSerial No`
+            where item_code = %s and status = 'Active' and name in (%s)
+        """
+            % ("%s", ", ".join(["%s"] * len(serial_nos))),
+            tuple([item_code] + serial_nos),
+            as_list=True,
+        )
+        valid_numbers = [d[0] for d in valid_numbers]
+        for d in serial_nos:
+            if not d in valid_numbers:
+                frappe.throw(
+                    "Invalid serial numbers %s for item %s"
+                    % (frappe.bold(serial_no), frappe.bold(item_code))
+                )
+
     def on_submit(self):
         if self.item_returned:
             self.make_stock_entry_for_item_returned()
@@ -35,6 +76,8 @@ class ItemReplacementCT(Document):
                         "qty": d.qty,
                         "expense_account": difference_account,
                         "allow_zero_valuation_rate": 1,
+                        "serial_no": d.serial_no,
+                        "batch_no": d.batch_no,
                     },
                 )
         stock_entry.set_stock_entry_type()
@@ -42,7 +85,6 @@ class ItemReplacementCT(Document):
         stock_entry.submit()
         for d in self.item_returned:
             d.reference_ste = stock_entry.name
-
 
     def make_stock_entry_for_item_issued(self):
         company = get_default_company()
@@ -63,6 +105,8 @@ class ItemReplacementCT(Document):
                         "qty": d.qty,
                         "expense_account": difference_account,
                         "allow_zero_valuation_rate": 1,
+                        "serial_no": d.serial_no,
+                        "batch_no": d.batch_no,
                     },
                 )
         stock_entry.set_stock_entry_type()
@@ -70,6 +114,3 @@ class ItemReplacementCT(Document):
         stock_entry.submit()
         for d in self.item_issued:
             d.reference_ste = stock_entry.name
-
-
-
